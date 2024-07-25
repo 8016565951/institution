d7f7036df0218fb541c9e81a86cc86f02dda86d3
itsdrvgo <thedragoluca@gmail.com>
2024-07-25 16:25:06 +0530

    FEAT: added new consts for course and blog default thumbnail paths

--- app/config/const.js
+++ app/config/const.js
@@ -3,6 +3,11 @@ const PORT = process.env.PORT ?? 3001;
 const AUTH_TOKEN_COOKIE_NAME = "institution__auth_SDRFasdrfa_532125";
 
 const DEFAULT_AVATAR_PATH = "uploads/images/avatars/default_avatar.png";
+const DEFAULT_COURSE_THUMBNAIL_PATH =
+    "uploads/images/courses/default_thumbnail.png";
+const DEFAULT_BLOG_THUMBNAIL_PATH =
+    "uploads/images/blogs/default_thumbnail.png";
+
 const ACCEPTED_IMAGE_TYPES = ["image/png", "image/jpg", "image/jpeg"];
 
 const JWT_EXPIRES_IN = "1d";
@@ -21,12 +26,22 @@ const ROLES = {
     ADMIN: "admin",
 };
 
+const TIMESPANS = {
+    DAY: "days",
+    WEEK: "weeks",
+    MONTH: "months",
+    YEAR: "years",
+};
+
 module.exports = {
     PORT,
     AUTH_TOKEN_COOKIE_NAME,
-    DEFAULT_AVATAR_PATH,
     JWT_EXPIRES_IN,
     cookieOptions,
     ROLES,
     ACCEPTED_IMAGE_TYPES,
+    DEFAULT_AVATAR_PATH,
+    DEFAULT_COURSE_THUMBNAIL_PATH,
+    DEFAULT_BLOG_THUMBNAIL_PATH,
+    TIMESPANS,
 };
