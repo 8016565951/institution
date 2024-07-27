4dd33d5fb6daba949dcd358ef0a2f83d46692914
Subha <survasurva246@gmail.com>
2024-07-27 11:25:35 +0530

    update ; admin pannel

--- app/controllers/admin/adminhome.controller.js
+++ app/controllers/admin/adminhome.controller.js
@@ -3,8 +3,33 @@ class adminHomeController {
         res.render("admin/index");
     };
 
-    home = async (req, res) => {
-        res.render("admin/home");
+    hero = async (req, res) => {
+        res.render("admin/hero");
+    };
+
+    courses = async (req, res) => {
+        res.render("admin/courses");
+    };
+
+    gallery = async (req, res) => {
+        res.render("admin/gallery");
+    };
+
+    students = async (req, res) => {
+        res.render("admin/students");
+    };
+
+    teachers = async (req, res) => {
+        res.render("admin/teachers");
+    };
+    about = async (req, res) => {
+        res.render("admin/about");
+    };
+    blogs = async (req, res) => {
+        res.render("admin/blogs");
+    };
+    contacts = async (req, res) => {
+        res.render("admin/contacts");
     };
 }
 
--- app/routes/admin/adminHome.routes.js
+++ app/routes/admin/adminHome.routes.js
@@ -3,6 +3,13 @@ const { adminHomeController } = require("../../controllers/admin");
 const Router = express.Router();
 
 Router.get("/", adminHomeController.index);
-Router.get("/home", adminHomeController.home);
+Router.get("/home", adminHomeController.hero);
+Router.get("/courses", adminHomeController.courses);
+Router.get("/gallery", adminHomeController.gallery);
+Router.get("/students", adminHomeController.students);
+Router.get("/teachers", adminHomeController.teachers);
+Router.get("/about", adminHomeController.about);
+Router.get("/blogs", adminHomeController.blogs);
+Router.get("/contacts", adminHomeController.contacts);
 
 module.exports = Router;
